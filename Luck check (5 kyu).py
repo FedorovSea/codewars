756cfad259a635701cba65492a886e1f6ada89a1
FedorovSea <fedorovseapy@gmail.com>
2022-06-28 21:05:48 +0300

    working v2

--- Luck check (5 kyu).py
+++ Luck check (5 kyu).py
@@ -1,6 +1,7 @@
 def luck_check(string):
     rang = int(len(string)/2)
-    if len(string)%2 == 0:
-        return sum(int(x) for x in string[:int(rang)]) == sum(int(z) for z in string[:int(rang-1):-1])
-    else:
-        return sum(int(x) for x in string[:int(rang)]) == sum(int(z) for z in string[:int(rang):-1])
+    return sum(map(int, string[:rang])) == sum(map(int, string[-rang:]))
+
+
+
+
