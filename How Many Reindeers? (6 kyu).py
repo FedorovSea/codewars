c9626d0d9c51aadbe976c484e5e0b6a54d1c91ae
FedorovSea <fedorovseapy@gmail.com>
2022-06-28 20:16:21 +0300

    working

--- How Many Reindeers? (6 kyu).py
+++ How Many Reindeers? (6 kyu).py
@@ -1,9 +1,11 @@
 import math
+
+
 def reindeer(presents):
-    try:
-        if presents < 180:
-            return math.ceil(presents / 30) + 2
-    except ValueError('Error'):
-        return 'Threw an error!'
+    if presents > 180:
+        raise ValueError()
+    return math.ceil(presents / 30) + 2
+
+
 
 print(reindeer(181))
