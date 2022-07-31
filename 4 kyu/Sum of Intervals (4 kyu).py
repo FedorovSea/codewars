2d05c30e9923eea3077a1fa92f17cfce7897efe1
FedorovSea <fedorovseapy@gmail.com>
2022-08-01 00:36:40 +0300

    version 2

--- 4 kyu/Sum of Intervals (4 kyu).py
+++ 4 kyu/Sum of Intervals (4 kyu).py
@@ -1,11 +1,6 @@
 def sum_of_intervals(intervals):
-    res = []
-    for i in intervals:
-        ranger = ''.join(str(i))
-        a,b = ranger.split()
-        a = a.lstrip('(').rstrip(',')
-        b = b.rstrip(')')
-        for ran in range(int(a)+1, int(b)+1):
-            if ran not in res:
-                res.append(ran)
+    res = set()
+    for start,stop in intervals:
+        for el in range(start, stop):
+            res.add(el)
     return len(res)
